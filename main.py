e6f514345ff9c07d3942adf4468fa1ea75750e21
Willian Binda <willianbinda122@gmail.com>
2026-06-15 21:03:36 -0300

    adicionando rotas da interface

--- main.py
+++ main.py
@@ -14,9 +14,49 @@ def buscarDadosTodosCliente():
 
     dados = consolidar_dados()
 
-    redis_client.set(
-        "dadosTodosClientes",
-        json.dumps(dados)
-    )
+    redis_client.set("dadosTodosClientes", json.dumps(dados))
+
+    return json.loads(dados)
+
+@app.get("/buscarClientesAmigos")
+def buscarClientesAmigos():
+    
+    dados_json = redis_client.get("clientesAmigos")
+
+    if dados_json:
+        return json.loads(dados_json)
+
+    dados = buscarDadosClientesAmigos()
+
+    redis_client.set("clientesAmigos", json.dumps(dados))
+
+    return json.loads(dados)
+
+@app.get("/buscarClientesCompra")
+def buscarClientesCompra():
+    
+    dados_json = redis_client.get("clientesCompra")
+
+    if dados_json:
+        return json.loads(dados_json)
+
+    dados = buscarDadosClientesCompra()
+
+    redis_client.set("clientesCompra", json.dumps(dados))
+
+    return json.loads(dados)
+
+@app.get("/buscarAmigosClienteRecomendacao")
+def buscarAmigosClienteRecomendacao():
+    
+    dados_json = redis_client.get("amigoClienteRecomendacao")
+
+    if dados_json:
+        return json.loads(dados_json)
+
+    dados = buscarDadosAmigosClienteRecomendacao()
+
+    redis_client.set("amigoClienteRecomendacao", json.dumps(dados))
+
     return json.loads(dados)
 
